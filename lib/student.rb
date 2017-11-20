ad19a1cc382f1bc7761407a24ca1d8b541c571dc
Joshua Needham <joshuabneedham@gmail.com>
2017-11-20 16:53:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,5 @@
 class Student < User
-  attr_accessor :knowledge
 
-  def initialize(knowledge)
-    @first_name
-    @last_name
-    @knowledge = []
-  end
+  KNOWLEDGE = []
 
-  def learn(knowledge)
-    @knowledge << knowledge
-  end
-
-  def knowledge
-    @knowledge
-  end
 end
